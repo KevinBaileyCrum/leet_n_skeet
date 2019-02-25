84f24ff87fa26ff3488fe713bddfc245ad4e88c5
Kevin Crum <kevinbaileycrum@gmail.com>
2019-02-25 15:50:19 -0800

    empty string and single char string success

--- longest-substring-without-repeating-characters/solution.py
+++ longest-substring-without-repeating-characters/solution.py
@@ -1,22 +1,26 @@
+# Kevin Crum
 # finds the longest substring without repeating characters
+# https://leetcode.com/problems/longest-substring-without-repeating-characters/
+# input data from infile presented in a comma seperated list of form
+#       string, expected_integer_length_substring
 
 def lengthOfLongestSubstring(s: 'str') -> 'int':
-    max = 0
-    count = 0
-    lookup_set = set()
-    for char in s:
-        if char in lookup_set:
-            if count > max:
-                max = count
-            lookup_set.clear()
-            count = 0
-        lookup_set.add(char)
-        count += 1
-
-    if count > max:
-        return count
-    return max
-
+    max_streak = 0
+    current_streak = 0
+    lookup_set = {}
+    beg = 0
+    current = 0
+    while current < len(s):
+        if s[current] in lookup_set:
+            if current_streak > max_streak:
+                max_streak = current_streak
+        else:
+            lookup_set[s[current]] = current
+            current_streak += 1
+        current += 1
+    if current_streak > max_streak:
+        max_streak = current_streak
+    return max_streak
 
 if __name__ == "__main__":
     with open ('infile.in') as fin:
