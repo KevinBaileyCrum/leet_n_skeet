387aca5b8b2472fa7668d655b7781becfc933177
Kevin Crum <kevinbaileycrum@gmail.com>
2019-02-28 14:06:59 -0800

    submission accepted.  only like 70 something percent though

--- longest-substring-without-repeating-characters/solution.py
+++ longest-substring-without-repeating-characters/solution.py
@@ -10,43 +10,17 @@ def lengthOfLongestSubstring(s: 'str') -> 'int':
     lookup_set = {}
     beg = 0
     current = 0
-    # while current < len(s):
-
-    #     print("beg %s current %s \n current_streak %s max_streak %s"%(beg, current, current_streak, max_streak))
-    #     # TODO: must clear from lookup table
-
-    #     if s[current] in lookup_set:
-    #         if current_streak > max_streak:
-    #             print("inner beg: %s, current: %s"%(beg, current))
-    #             max_streak = current_streak
-    #         beg = lookup_set[s[current]] + 1
-    #         current_streak = current - beg
-    #     else:
-    #         current_streak += 1
-    #     lookup_set[s[current]] = current
-    #     current += 1
-    # if current_streak > max_streak:
-    #     print("outer beg: %s, current: %s"%(beg, current))
-    #     max_streak = current_streak
-    # return max_streak
     while current < len(s):
-        # print("curr-> %s beg %s current %s \n current_streak %s max_streak %s"%(s[current], beg, current, current_streak, max_streak))
-        # print(lookup_set)
         if s[current] in lookup_set:
             end_letter = lookup_set[s[current]]
             if current_streak > max_streak:
                 max_streak = current_streak
             while beg <= end_letter:
-                # print(lookup_set[s[current]])
-                # print("deleting %s"%(lookup_set[s[beg]]))
                 del lookup_set[s[beg]]
                 beg += 1
                 current_streak -= 1
         lookup_set[s[current]] = current
         current_streak += 1
-        # print("lookup_set[s[current]]"%(lookup_set[s[current]]))
-        #print(lookup_set[s[current]])
-        #print(s[current])
         current += 1
 
     if current_streak > max_streak:
