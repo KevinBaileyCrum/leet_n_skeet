87d8edb085055a1ee3ce7ec68266914f5b1b4bce
Kevin Crum <kevinbaileycrum@gmail.com>
2019-03-04 17:56:41 -0800

    i think i may be done

--- median_two_sorted_arrays/solution.py
+++ median_two_sorted_arrays/solution.py
@@ -1,91 +1,31 @@
 from sys import argv
 
-# def return_min(nums1=None, nums2=None,p1=None, p2=None):
-#     if p1 is not None and p2 is not None:
-#         if nums1[p1] < nums2[p2]:
-#             avg1 = nums1[p1]
-#             p1 += 1
-#         else:
-#             avg1 = nums2[p2]
-#             p2 += 1
-#         if nums1[p1] < nums2[p2]:
-#             avg2 = nums1[p1]
-#         else:
-#             avg2 = nums2[p2]
-#     elif p1 is not None and p2 is None:
-#         avg1 = nums1[p1]
-#         avg2 = nums1[p1+1]
-#     else:
-#         avg1 = nums2[p2]
-#         avg2 = nums2[p2+1]
-#     return (avg1 + avg2) / 2
-
-
-# def findMedianSortedArrays(nums1, nums2):
-#     half_iteratoins = ((len(nums1) + len(nums2)) / 2) - 1
-#     is_even = (len(nums1) + len(nums2)) % 2 == 0
-#     p1 = 0
-#     p2 = 0
-#     print("top")
-#     while p1 < len(nums1) and p2 < len(num2):
-#         print('inner both')
-#         if p1 + p2 < half_iteratoins:
-#             if nums1[p1] < nums2[p2]:
-#                p1 += 1
-#             else:
-#                p2 += 1
-#         elif not is_even:
-#             if nums1[p1] < nums2[p2]:
-#                 return nums1[p1]
-#             else:
-#                 return nums2[p2]
-#         else:
-#             return_min(nums1, nums2, p1, p2)
-#     while p1 < len(nums1):
-#         print('inner 1')
-#         if p1 + p2 < half_iteratoins:
-#             # optimization extrapolate median here
-#             p1 += 1
-#         elif not is_even:
-#             return nums1[p1]
-#         else:
-#             return return_min(nums1, None, p1, None)
-#     while p2 < len(nums2):
-#         print('inner 2')
-#         if  p1 + p2 < half_iteratoins:
-#             p2 += 1
-#         elif not is_even:
-#             return nums2[p2]
-#         else:
-#             return return_min(None, nums2, None, p2)
-#     return -1
-
 def get_next(nums1, p1, nums2, p2):
     less_than = -1
     if p1 < len(nums1) and p2 < len(nums2):
         if nums1[p1] < nums2[p2]:
             less_than = nums1[p1]
             p1 += 1
+        else:
+            less_than = nums2[p2]
+            p2 += 1
     elif p1 < len(nums1):
         less_than = nums1[p1]
         p1 += 1
     elif p2 < len(nums2):
         less_than = nums2[p2]
         p2 += 1
-    print("returning p1 %s, p2 %s"%(p1, p2))
-    return less_than
+    return float(less_than), p1, p2
 
 def findMedianSortedArrays(nums1, nums2):
-    half_iterations = ((len(nums1) + len(nums2)) / 2) -1
+    half_iterations = ((len(nums1) + len(nums2)) / 2)
     is_even = (len(nums1) + len(nums2)) % 2 == 0
     p1 = 0
     p2 = 0
     while p1 + p2 < half_iterations:
-        print("before p1 %s, p2 %s"%(p1, p2))
-        curr = get_next(nums1, p1, nums2, p2)
-        print("after p1 %s, p2 %s"%(p1, p2))
+        curr, p1, p2 = get_next(nums1, p1, nums2, p2)
     if is_even:
-        curr_next = get_next(nums1, p1, nums2, p2)
+        curr_next, p1, p2 = get_next(nums1, p1, nums2, p2)
         curr = (curr_next + curr) / 2
     return curr
 
@@ -97,10 +37,11 @@ if __name__ == "__main__":
             num1 = num1.split()
             num2 = num2.split()
             expected = expected.strip()
+            print(line)
             solution = findMedianSortedArrays(num1, num2)
             if str(solution) == expected:
                 print('success')
             else:
                 print('failure')
-            print("line %s expected %s solution %s"%(line, expected, solution))
+            print("line %s expected %s solution %s\n"%(line, expected, solution))
 
