ce42b8144fc232f1ba6b21983920a680cc196d7f
Kevin Crum <kevinbaileycrum@gmail.com>
2019-09-11 14:44:45 -0700

    catches overflow

--- reverse_integer/solution.c
+++ reverse_integer/solution.c
@@ -1,26 +1,33 @@
 #include <stdio.h>
 #include <math.h>
+#include <limits.h>
 
 int reverse(int x)
 {
+  long n = x;
   int isNeg = 0;
-  if(x<0)
+  if (n<0)
   {
     isNeg = 1;
-    x *= -1;
+    n *= -1;
   }
-  int power = (int) log10(x);
-  int accum = 0;
-  for(int i=0; i<=power; ++i)
+  int power = (int) log10(n);
+  long accum = 0;
+  for (int i=0; i<=power; ++i)
   {
-    int curr = x % (int)pow(10,i+1); 
+    long curr = n % (int)pow(10,i+1); 
     curr = curr / (int)pow(10,i);
     accum += curr*(int)pow(10, power-i);
   }
-  return isNeg?accum*=-1:accum;
+  if (accum > INT_MAX){
+    return 0;
+  }
+  if (isNeg) accum*=-1;
+  return (int)accum;
 }
 
 int main()
 {
-  reverse(1234);
+  printf("%d\n",reverse(6789));
+  printf("%u\n",reverse(1534236469));
 }
