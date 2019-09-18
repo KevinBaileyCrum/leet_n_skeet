97614797d442ee2c12d5297fa7d9ddbd08e68fad
Kevin Crum <kevinbaileycrum@gmail.com>
2019-09-18 14:30:03 -0700

    done but string conversion solutions were still faster. my guess the test cases werent big enough

--- palindrome_number/solution.py
+++ palindrome_number/solution.py
@@ -1,20 +1,33 @@
 from sys import argv
+import math
 
 def isPalindrome(x: int) -> bool:
-    False
+    while(x > 0):
+        power = int(math.log10(x))
+        i=0
+        while i<=math.ceil(power/2):
+            if int( x / 10 ** (power - i*2) ) == x % 10:
+                x = x % 10 ** (power - (i*2))
+                x = int(x/10)
+                i += 1
+            else:
+                return False
+        return True
+    if x == 0:
+        return True
+    else:
+        return False
 
 if __name__ == "__main__":
     with open (argv[1]) as fin:
         for line in fin:
             line = line.strip()
             num, expected = line.split()
+            print('\n')
             print(line)
-            solution = isPalindrome(num)
+            solution = isPalindrome(int(num))
             if str(solution) == expected:
                 print('success')
             else:
                 print('failure')
-            print("line %s expected %s solution %s\n"%(line, expected, solution))
-
-            print("num is %s expected is %s"%(num, expected))
 
