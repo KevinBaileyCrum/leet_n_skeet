b61562ddfeb4ac5b155c36440d85e056352fb955
Kevin Crum <kevinbaileycrum@gmail.com>
2019-09-25 16:08:22 -0700

    done 100% better on space hi 60s for runtime

--- atoi/solution.c
+++ atoi/solution.c
@@ -1,52 +1,44 @@
 #include <stdio.h>
-#include <stdlib.h>
+#define  int_max 2147483647
+#define  int_min -2147483648
 
 int Atoi(char *str){
-  
-  char solution[99];
-  int i=0;
-
-  int isNeg = 0;
+  long number = 0;
+  int sign = 1;
   // trim whitespace
   while(*str && *str == ' '){ 
     str++;
   }
-  printf("str now %s\n", str);
   // get sign
-  if(*str && *str == '-'){
-    isNeg++;
+  if(*str && (*str == '-' || *str == '+')){
+    if(*str == '-') sign = -1;
     str++;
   }
   // get numbers until invalid input
   while(*str && *str >= '0' && *str <= '9'){
-    solution[i] = *str;
-    i++;
+    if(sign*number > int_max) return int_max;
+    if(sign*number < int_min) return int_min;
+    number = (*str-'0') + (number*10); // subtract 48
     str++;
   }
   // return
-  printf("solution is %s\n", solution);
-  return 1;
-}
-
-int a2i(const char *s)
-{
- int sign=1;
- if(*s == '-')
-        sign = -1;
- s++;
- int num=0;
- while(*s)
-  {
-    num=((*s)-'0')+num*10;
-    s++;
-  }
- return num*sign;
+  number *=sign;
+  if(number>int_max) return int_max;
+  if(number<int_min) return int_min;
+  return (int)number; 
 }
 
 int main(){
-  printf("%d\n",Atoi("1234"));
-  printf("%d\n",Atoi("-1234"));
+  printf("%d\n",Atoi(" these are words 1234"));
+  printf("%d\n",Atoi("-1234 these are words"));
   printf("%d\n",Atoi("\0"));
   printf("%d\n",Atoi("          1234       "));
+  printf("%d\n",Atoi("12"));
+  printf("%d\n",Atoi("2147483647"));
+  printf("%d\n",Atoi("-2147483648"));
+  printf("%d\n",Atoi("2147483648"));  // ret int max
+  printf("%d\n",Atoi("-2147483649")); // ret int min
+  printf("%d\n",Atoi("999999999990000")); // ret int max
+  printf("%d\n",Atoi("12"));
   /* printf("%d\n",a2i("1234")); */
 }
