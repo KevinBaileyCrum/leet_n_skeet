fd4f63f5abdd1c34956bd3c4120cdd6466cb9dbc
Kevin Crum <kevinbaileycrum@gmail.com>
2019-09-25 22:38:12 -0700

    done one easy in one sit down

--- valid_parentheses/solution.py
+++ valid_parentheses/solution.py
@@ -1,5 +1,21 @@
 def isValid(str):
-    pass
+    stack = []
+    opening = ['[', '{',  '(']
+    closing = [']', '}', ')']
+    for char in str:
+        if char in opening:
+            stack.append(char)
+        if char in closing:
+            if(len(stack) < 1 or opening.index(stack.pop()) != closing.index(char)):
+                return False
+    if len(stack) != 0:
+        return False
+    return True
 
-if __name__ == "__main__":
-    isValid("()")
+if __name__ == '__main__':
+    print(isValid('()'))
+    print(isValid(')'))
+    print(isValid('('))
+    print(isValid('([])'))
+    print(isValid('({}[]())'))
+    print(isValid('({}[])())'))
