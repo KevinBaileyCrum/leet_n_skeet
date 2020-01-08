a5742966ec0508451cc9955014baff5df072c0c3
Kevin Crum <kevinbaileycrum@gmail.com>
2020-01-07 20:56:57 -0800

    pythonic substr helper

--- substr_anagram/solution.py
+++ substr_anagram/solution.py
@@ -1,20 +1,25 @@
 # Python3 program to print all possible
 # substrings of a given string
+import string
 
 # Function to print all sub strings
 def subString(s):
     substrList = []
-    i = 0
-    substrList.append(s)
-    while i < len(s):
+    count = 0
+    for i in range(len(s)):
         j=i+1
-        while j < len(s):
-            substrList.append(s[i:j])
+        while j <= len(s):
+            count += 1
+            substrList.append((count, s[i:j]))
             j+=1
         i+=1
     return substrList
 
 # Driver program to test above function
-s = 'abcd'
-print(subString(s))
+s0 = 'abcd'
+s1 = 'mom'
+s2 = string.ascii_lowercase
+s3 = 'z'
+print(subString(s1))
+
 
