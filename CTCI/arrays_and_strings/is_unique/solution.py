6a99b62c59a5d323c59a69c9f7798a7c3e0a5fb7
Kevin Crum <kevinbaileycrum@gmail.com>
2020-01-11 13:11:30 -0800

    without data structures

--- CTCI/arrays_and_strings/is_unique/solution.py
+++ CTCI/arrays_and_strings/is_unique/solution.py
@@ -1,5 +1,6 @@
 #algorithm that determines if a string has all unique char
 
+# with additional data stuctures
 def isUnique(string):
     lookUp = set()
     for char in string:
@@ -7,6 +8,16 @@ def isUnique(string):
             lookUp.add(char)
         else:
             return False
+    return True
+
+# without additional data stuctures
+def isUniqueRaw(string):
+    s = sorted(string)
+    prev = string[0]
+    for i in range(1, len(s)):
+        if string[i] == prev:
+            return False
+        prev = string[i]
     return True
 
 if __name__ == '__main__':
@@ -18,5 +29,6 @@ if __name__ == '__main__':
 
     for string, expected in stringList:
         if isUnique(string) != expected:
-            print('error: string={} expected={}'.format(string, expected))
-
+            print('isUnique error: string={} expected={}'.format(string, expected))
+        if isUniqueRaw(string) != expected:
+            print('isUniqueRaw error: string={} expected={}'.format(string, expected))
