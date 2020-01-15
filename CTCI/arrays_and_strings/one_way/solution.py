1982ac7043f6020b2b5be67cc6ba9f2768993e12
KevinBaileyCrum <kevinbaileycrum@gmail.com>
2020-01-14 21:23:32 -0800

    completed one_away to the best of my testing

--- CTCI/arrays_and_strings/one_way/solution.py
+++ CTCI/arrays_and_strings/one_way/solution.py
@@ -7,7 +7,26 @@
 
 def is_one_away(s1, s2):
     if (max(len(s1), len(s2)) - min(len(s1), len(s2)) <= 1):
-        print('hello')
+        lookUpDict = {}
+        for char in s1:
+            if char in lookUpDict:
+                lookUpDict[char] += 1
+            else:
+                lookUpDict[char] = 1
+
+        notFoundBuffer = []
+        for char in s2:
+            if char in lookUpDict:
+                lookUpDict[char] -= 1
+                if lookUpDict[char] == 0:
+                    del lookUpDict[char]
+            else:
+                notFoundBuffer.append(char)
+
+        # print('len lookUpDict {} len notFoundBuffer {}'.format(len(lookUpDict), len(notFoundBuffer)))
+        if len(lookUpDict) == 0 or len(lookUpDict) == 1:
+            if len(notFoundBuffer) == 0 or len(notFoundBuffer) == 1:
+                return True
     return False
 
 if __name__ == '__main__':
@@ -15,9 +34,12 @@ if __name__ == '__main__':
         ['pale', 'paleid', False],
         ['pale', 'ple', True],
         ['pales', 'pale', True],
+        ['pale', 'pales', True],
         ['pale', 'bale', True],
+        ['pale', 'pales', True],
         ['pale', 'bake', False],
-        ['pale', 'pale', False]
+        ['pale', 'pale', True],
+        ['paleid', 'pale', False]
     ]
 
     for s1, s2, expected in stringsList:
