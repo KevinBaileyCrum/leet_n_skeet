155f3e87a1a4f1da1aa8fa52fa6f9c93dba949d2
Kevin Crum <kevinbaileycrum@gmail.com>
2020-04-19 15:26:08 -0700

    solution arrived

--- print_in_order_NOTDONE/solution.cpp
+++ print_in_order_NOTDONE/solution.cpp
@@ -1,46 +1,82 @@
-#import <thread>
-#import <iostream>
-#import <vector>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <iostream>
+#include <vector>
 
 class Foo {
-  public:
-    Foo() {
+   private:
+      void printFirst() {
+         std::cout << "first";
+      }
 
-    }
+      void printSecond() {
+         std::cout << "second";
+      }
 
-    void first(std::function<void()> printFirst) {
+      void printThird() {
+         std::cout << "third";
+      }
 
-      // printFirst() outputs "first". Do not change or remove this line.
-      printFirst();
-    }
+   public:
+         int count;
+         std::mutex mtx;
+         std::condition_variable condVar;
+      Foo() {
+         count = 1;
+      }
 
-    void second(std::function<void()> printSecond) {
+      void first() {
+         std::unique_lock<std::mutex> lck(mtx);
+         std::cout << "first";
+         ++count;
+         condVar.notify_all();
+         // printFirst() outputs "first". Do not change or remove this line.
+         /* printFirst(); */
+      }
 
-      // printSecond() outputs "second". Do not change or remove this line.
-      printSecond();
-    }
+      void second() {
+         std::unique_lock<std::mutex> lck(mtx);
+         condVar.wait(lck, [this]() { return count == 2;});
+         std::cout << "second";
+         ++count;
+         condVar.notify_all();
+         // printSecond() outputs "second". Do not change or remove this line.
+         /* printSecond(); */
+      }
 
-    void third(std::function<void()> printThird) {
-
-      // printThird() outputs "third". Do not change or remove this line.
-      printThird();
-    }
+      void third() {
+         std::unique_lock<std::mutex> lck(mtx);
+         condVar.wait(lck, [this]() { return count == 3;});
+         std::cout << "third";
+         ++count;
+         condVar.notify_all();
+         // printThird() outputs "third". Do not change or remove this line.
+         /* printThird(); */
+      }
 };
 
 int main(){
 
-  /* std::thread t1(printFirst); */
-  /* std::thread t2(printSecond); */
-  /* std::thread t3(printThird); */
+   /* std::thread t1(printFirst); */
+   /* std::thread t2(printSecond); */
+   /* std::thread t3(printThird); */
 
-  /* t1.join(); */
-  /* t2.join(); */
-  /* t3.join(); */
+   /* t1.join(); */
+   /* t2.join(); */
+   /* t3.join(); */
 
-  /* std::vector<int> input1 {1, 2, 3}; */
-  /* std::vector<int> input2 {3, 2, 1}; */
-  /* std::vector<int> input3 {2, 1, 1}; */
+   /* std::vector<int> input1 {1, 2, 3}; */
+   /* std::vector<int> input2 {3, 2, 1}; */
+   /* std::vector<int> input3 {2, 1, 1}; */
 
-  Foo fool;
-  std::thread t1(fool.first());
+   Foo fool;
+   std::thread t1(&Foo::first, &fool);
+   std::thread t2(&Foo::second, &fool);
+   std::thread t3(&Foo::third, &fool);
+
+   t3.join();
+   t2.join();
+   t1.join();
+
 }
