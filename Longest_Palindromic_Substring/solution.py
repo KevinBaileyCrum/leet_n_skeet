83c8516b7599ee04dce58e49975b5e0350759ecc
Kevin Crum <kevinbaileycrum@gmail.com>
2020-05-15 14:28:53 -0700

    nasty brute force

--- Longest_Palindromic_Substring/solution.py
+++ Longest_Palindromic_Substring/solution.py
@@ -1,5 +1,35 @@
+def getPalindromeLength(s: str) -> int:
+    start = 0
+    end = len(s) - 1
+
+    while start < end:
+        print(f'start: {start} end {end} s[start] {s[start]} s[end] {s[end]}')
+        if s[start] != s[end]:
+            return (0, '')
+        start += 1
+        end   -= 1
+
+    return (len(s), s)
+
 def longestPalindrome(s: str) -> str:
-    return "test"
+    print(s)
+    # iterate through every substring
+    max = (0, '')
+    i = 0
+    while i < len(s):
+        j = i
+        while j < len(s):
+            print({s[i:j+1]})
+            # determine length of palindrome in substring
+            # print(palindromeLength(s[i:j+1]))
+            palindromeLength = getPalindromeLength(s[i:j+1])
+            # update max
+            if palindromeLength[0] > max[0]:
+                max = palindromeLength
+            j += 1
+        i += 1
+
+    return max[1]
 
 if __name__ == "__main__":
     with open ('infile.in') as fin:
